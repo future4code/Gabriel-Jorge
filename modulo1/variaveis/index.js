db3d754db30dae108ea9495bfb82cb56371d83a3
Gabriel Jorge <gabriel.ggcj@gmail.com>
2021-10-04 18:53:29 -0300

    executei atividade do dia imcompleta

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,25 @@
+//Exercicio de interpretação ao codigo
+
+/*let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+
+let valorDiaria = prompt("Quanto você recebe por dia?")
+
+alert(`Voce recebe r$ ${horasTrabalhadas/valorDiaria} por hora`) */
+
+//Exercicio de Escrita do codigo
+
+/*let nomeDoUsuario = prompt ("Qual é seu nome amiguinho ???")
+let idadeDoUsuario = prompt ("Qual a sua idade amiguinho ???")
+console.log(typeof nomeDoUsuario)
+console.log(typeof idadeDoUsuario)*/
+
+//ele foi impresso destro do Campo de Inspeção 
+
+/*console.log("Olá, ",nomeDoUsuario," você tem " ,idadeDoUsuario," anos de idade.")*/
+
+/*let gostaDeRock = prompt ("Você gosta de rock ???")
+console.log (gostaDeRock)
+let gostaDeSamba = prompt ("Você gosta de samba ???")
+console.log (gostaDeSamba)
+let gostaDeFunk = prompt ("Gosta de Funk ???")
+console.log (gostaDeFunk)*/
