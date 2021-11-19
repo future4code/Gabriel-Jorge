bf5e2f68485a7a6edb9308ddd75bd5477d34093b
Gabriel Jorge <gabriel.ggcj@gmail.com>
2021-11-18 22:14:29 -0300

    ajuste de git

---
+++ modulo2/projeto-labefy/labefy/src/App.js
@@ -0,0 +1,23 @@
+import react from "react";
+
+export default class PaginaInicial extends react.Component{
+  state = {
+    telaAtual: ""
+  }
+  
+
+  escolheTela = () => {
+
+  
+  }
+
+
+render(){
+  return(
+    <div>olá mundo</div>
+  )
+
+}
+
+
+}
