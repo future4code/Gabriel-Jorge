cc7d0b3b24f2ffc6e7d59e74327adfe21440c59b
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-01-30 23:12:55 -0300

    Exercicio Sistema bancario Mark-1

---
+++ modulo4/sistama-bancario/gb_bank/src/users.ts
@@ -0,0 +1,21 @@
+export const users: Users[] = [
+
+{name: "Gabriel Jorge", cpf: "341.413.345-43", dateBirth: "24/9/1993", sex: "mas" , balance: 200 },
+{name: "Marcelina Telefone", cpf: "424.132.645-23", dateBirth: "12/9/1998", sex: "fem" , balance: 344},
+{name: "Aroldo de Boa", cpf: "123.433.745-13", dateBirth: "14/3/1967", sex: "mas" , balance: 5455},
+{name: "Fulana de Boa", cpf: "231.342.523-54", dateBirth: "12/2/1996", sex: "fem" , balance: 5534},
+{name: "Maria de Boa", cpf: "423.234.423-23", dateBirth: "11/4/1981", sex: "fem" , balance: 434},
+{name: "Mauricio de Souza", cpf: "123.345.456-12", dateBirth: "08/9/1992", sex: "mas" , balance: 999 },
+{name: "Jordan Bernardes", cpf: "323.435.765-65", dateBirth: "01/9/1991", sex: "mas" , balance: 675},
+{name: "Mauricio de Souza", cpf: "674.545.565-52", dateBirth: "12/1/1997", sex: "mas" , balance: 464564},
+{name: "Luana de Souza", cpf: "174.515.265-12", dateBirth: "21/11/1993", sex: "fem" , balance: 6576 },
+{name: "Arnaldo Pinto", cpf: "134.215.325-12", dateBirth: "22/10/1992", sex: "mas" , balance: 6575}
+]
+
+type Users = {
+name: string,
+cpf: string,
+dateBirth: string,
+sex: string,
+balance: number
+}
